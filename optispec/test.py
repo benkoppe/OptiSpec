c3c2e347bd4210927cd295cc9e76f936b77f3f23
benkoppe <bentastic1@gmail.com>
2024-08-15 16:01:45 -0700

    added temp test function for timing matrix

--- optispec/test.py
+++ optispec/test.py
@@ -1,5 +1,6 @@
 import jax
 import jax.numpy as jnp
+import numpy as np
 import random
 import time
 
@@ -7,12 +8,12 @@ from optispec.models import two_state as ts
 
 # display sample usage of two-state model
 
-jax.config.update("jax_platform_name", "gpu")
-# jax.config.update("jax_enable_x64", True)
+jax.config.update("jax_platform_name", "cpu")
+jax.config.update("jax_enable_x64", True)
 
 
 def time_function():
-    p = ts.Params(transfer_integral=random.randint(90, 110))
+    p = ts.Params(transfer_integral=random.randint(90, 110), mode_basis_sets=(40, 200))
 
     start = time.time()
 
@@ -25,13 +26,26 @@ def time_function():
     return end - start, peaks
 
 
+def time_matrix():
+    p = ts.Params(transfer_integral=random.randint(90, 110))
+
+    start = time.time()
+
+    ts.diagonalize(p)
+
+    end = time.time()
+
+    return end - start
+
+
 N = 10
 runtimes = []
 
 for _ in range(N):
-    runtime, peaks = time_function()
+    runtime = time_matrix()
     print(runtime)
-    # print(peaks)
     runtimes.append(runtime)
 
-print(f"Average runtime: {sum(runtimes[1:]) / (N - 1):.2f} seconds")
+
+print(f"Average runtime: {np.mean(runtimes):.2f} seconds")
+print(f"Average runtime excluding first: {np.mean(runtimes[1:]):.2f} seconds")
