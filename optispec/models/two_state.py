9ff332b08e954055881fb03f390c691368dd0e88
benkoppe <bentastic1@gmail.com>
2024-08-15 05:04:57 -0700

    added jit decorators

--- optispec/models/two_state.py
+++ optispec/models/two_state.py
@@ -22,14 +22,17 @@ class Params(CommonParams):
     mode_basis_sets: jdc.Static[tuple[int, ...]] = (20, 200)
 
 
+@jdc.jit
 def diagonalize(params: Params) -> h.Diagonalization:
     return h.diagonalize(_hamiltonian_params(params))
 
 
+@jdc.jit
 def hamiltonian(params: Params) -> h.Matrix:
     return h.hamiltonian(_hamiltonian_params(params))
 
 
+@jdc.jit
 def _hamiltonian_params(params: Params) -> h.Params:
     return h.Params(
         transfer_integrals=params.transfer_integral,
@@ -39,3 +42,8 @@ def _hamiltonian_params(params: Params) -> h.Params:
         mode_frequencies=params.mode_frequencies,
         mode_state_couplings=jnp.array([[0.0, 0.0], params.mode_couplings]),
     )
+
+
+# broaden peaks
+
+# compute peaks
