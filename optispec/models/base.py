02c1160c78736538a401711372becd8801a5ff9d
benkoppe <bentastic1@gmail.com>
2024-08-15 04:11:18 -0700

    more verbose types for base classes

--- optispec/models/base.py
+++ optispec/models/base.py
@@ -4,12 +4,12 @@ from jaxtyping import Array, Float
 
 @jdc.pytree_dataclass
 class CommonParams:
-    start_energy: float = 0.0
-    end_energy: float = 20_000.0
-    num_points: int = 2_001
+    start_energy: jdc.Static[float] = 0.0
+    end_energy: jdc.Static[float] = 20_000.0
+    num_points: jdc.Static[int] = 2_001
 
 
 @jdc.pytree_dataclass
 class Spectrum:
-    energies: Float[Array, "num_points"]
-    intensities: Float[Array, "num_points"]
+    energies: Float[Array, " num_points"]
+    intensities: Float[Array, " num_points"]
