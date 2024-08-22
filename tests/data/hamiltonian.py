ae86b68804f693de683fbc4e212633adcd7093d8
benkoppe <bentastic1@gmail.com>
2024-08-22 14:57:10 -0400

    standardized data loading with functools.partial

--- tests/data/hamiltonian.py
+++ tests/data/hamiltonian.py
@@ -1,8 +1,11 @@
+from functools import partial
+
 from .load import load_csv
 
-subdir = "hamiltonian"
-_file_postfix = "t=100;e=0,8000;bs=9,10;f=1200,100;c=0p7,2"
+load_csv = partial(
+    load_csv, subdir="hamiltonian", params="t=100;e=0,8000;bs=9,10;f=1200,100;c=0p7,2"
+)
 
-OLD_PACKAGE_MATRIX = load_csv(subdir, f"matrix:{_file_postfix}")
-OLD_PACKAGE_EIGENVALUES = load_csv(subdir, f"vals:{_file_postfix}")
-OLD_PACKAGE_EIGENVECTORS = load_csv(subdir, f"vects:{_file_postfix}")
+OLD_PACKAGE_MATRIX = load_csv("matrix")
+OLD_PACKAGE_EIGENVALUES = load_csv("vals")
+OLD_PACKAGE_EIGENVECTORS = load_csv("vects")
--- tests/data/load.py
+++ tests/data/load.py
@@ -6,5 +6,5 @@ data_dir = Path(__file__).parent
 csv_dir = data_dir / "csv"
 
 
-def load_csv(subdir, name):
-    return np.loadtxt(csv_dir / subdir / f"{name}.csv")
+def load_csv(name, params, subdir):
+    return np.loadtxt(csv_dir / subdir / f"{name}:{params}.csv")
