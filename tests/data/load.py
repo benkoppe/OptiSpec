19c5dd153e0172acd22edec7afee100588444b1a
Ben <Koppe.Development@gmail.com>
2024-09-10 10:37:10 -0400

    modified load function to use JAX

--- tests/data/load.py
+++ tests/data/load.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 
 import numpy as np
+import jax.numpy as jnp
 
 data_dir = Path(__file__).parent
 csv_dir = data_dir / "csv"
 
 
 def load_csv(name, params, subdir):
-    return np.loadtxt(csv_dir / subdir / f"{name}:{params}.csv")
+    return jnp.array(np.loadtxt(csv_dir / subdir / f"{name}:{params}.csv"))
