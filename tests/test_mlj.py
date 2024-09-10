c5c5289603c1cb50acdadce585b686ea788dacc2
Ben <Koppe.Development@gmail.com>
2024-09-10 16:56:11 -0400

    written tests for future mlj model to fulfill

--- tests/test_mlj.py
+++ tests/test_mlj.py
@@ -1,1 +1,18 @@
 import pytest
+import jax.numpy as jnp
+
+from optispec.models import mlj
+from tests.data.mlj import OLD_PACKAGE_ABSORPTION
+
+
+@pytest.fixture
+def mlj_params():
+    return mlj.Params()
+
+
+def test_mlj_absorption_against_old_python_package(mlj_params):
+    params = mlj_params
+    absorption = mlj.absorption(params)
+
+    absorption = absorption.match_greaetest_peak_of(OLD_PACKAGE_ABSORPTION)
+    return jnp.allclose(absorption.intensities, OLD_PACKAGE_ABSORPTION)
