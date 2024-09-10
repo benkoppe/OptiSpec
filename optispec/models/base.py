50e24fd7315a13f64ce6abf21d4765751ae05b88
Ben <Koppe.Development@gmail.com>
2024-09-10 10:25:39 -0400

    implemented auxilliary helper functions for Spectrum

--- optispec/models/base.py
+++ optispec/models/base.py
@@ -1,3 +1,4 @@
+import jax.numpy as jnp
 import jax_dataclasses as jdc
 from jaxtyping import Array, Float
 
@@ -13,3 +14,20 @@ class CommonParams:
 class Spectrum:
     energies: Float[Array, " num_points"]
     intensities: Float[Array, " num_points"]
+
+    def energies_equal(self, other: "Spectrum") -> bool:
+        return jnp.array_equal(self.energies, other.energies).item()
+
+    def intensities_similar(self, other: "Spectrum", rtol=1e-05, atol=1e-08) -> bool:
+        return jnp.allclose(self.intensities, other.intensities, rtol=rtol, atol=atol).item()
+
+    def __mul__(self, other: Float) -> "Spectrum":
+        return Spectrum(self.energies, self.intensities * other)
+
+    def match_greatest_peak_of(
+        self, other_intensities: Float[Array, " num_points"]
+    ) -> "Spectrum":
+        max_intensity = self.intensities.max()
+        other_max_intensity = other_intensities.max()
+
+        return self * (other_max_intensity / max_intensity)
