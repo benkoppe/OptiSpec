cd933c3ba02a430de1f4220f542d8e69db961a1b
Ben <Koppe.Development@gmail.com>
2024-09-10 13:27:52 -0400

    properly test fortran output

--- tests/test_two_state.py
+++ tests/test_two_state.py
@@ -20,20 +20,6 @@ def two_state_params():
         mode_couplings=jnp.array([0.7, 2.0]),
     )
 
-@pytest.fixture
-def two_state_params_fortran():
-    return ts.Params(
-        start_energy=0.0,
-        end_energy=20_000.0,
-        num_points=2_001,
-        temperature_kelvin=300.0,
-        broadening=200.0,
-        transfer_integral=100.0,
-        energy_gap=8_000.0,
-        mode_basis_sets=(20, 200),
-        mode_frequencies=jnp.array([1200.0, 100.0]),
-        mode_couplings=jnp.array([0.7, 2.0]),
-    )
 
 def test_two_state_absorption_against_old_python_package(two_state_params):
     params = two_state_params
@@ -42,9 +28,9 @@ def test_two_state_absorption_against_old_python_package(two_state_params):
     absorption = absorption.match_greatest_peak_of(OLD_PACKAGE_ABSORPTION)
     assert jnp.allclose(absorption.intensities, OLD_PACKAGE_ABSORPTION)
 
-def test_two_state_absorption_against_fortran_code(two_state_params_fortran):
-    params = two_state_params_fortran
+def test_two_state_absorption_against_fortran_code(two_state_params):
+    params = two_state_params
     absorption = ts.absorption(params)
 
     absorption = absorption.match_greatest_peak_of(FORTRAN_CODE_ABSORPTION)
-    assert jnp.allclose(absorption.intensities, FORTRAN_CODE_ABSORPTION)
+    assert jnp.allclose(absorption.intensities, FORTRAN_CODE_ABSORPTION, atol=1e-6)
