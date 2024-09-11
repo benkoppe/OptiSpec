f0d3abfd839e9493b13eae978170bbc1acc9876c
Ben <Koppe.Development@gmail.com>
2024-09-11 14:55:32 -0400

    corrected file name

--- tests/data/mlj.py
+++ tests/data/mlj.py
@@ -5,7 +5,7 @@ from .load import load_csv
 load_csv = partial(
     load_csv,
     subdir="mlj",
-    params="points=0,20k,2001;temp=300;broad=200;t=100;e=8k;bs=20;50,f=1200,100;c=0p7,2",
+    params="points=0,20k,2001;temp=300;e=8k;d=0;bs=20,f=1200,100;c=0p7,2",
 )
 
 OLD_PACKAGE_ABSORPTION = load_csv("absorption-intensities")
